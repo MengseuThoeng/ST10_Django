152ab4502824b046af69d92582d9dcb2dd416acc
MengseuThoeng <mengseu2004@gmail.com>
2025-05-27 08:14:12 +0700

    Implement 3 Models and Register Admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from store.models import Orders, Products, Category
+
+
 # Register your models here.
+admin.site.register(Orders)
+admin.site.register(Products)
+admin.site.register(Category)
--- store/models.py
+++ store/models.py
@@ -2,13 +2,27 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        db_table = 'category'
+
+    def __str__(self):
+        return f"{self.name} - {self.description}"
+
+
 class Products(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     qty = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     class Meta:
         db_table = 'product'
 
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
 
 class Orders(models.Model):
     qty = models.IntegerField()
@@ -16,3 +30,6 @@ class Orders(models.Model):
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
     class Meta:
         db_table = 'order'
+
+    def __str__(self):
+        return f"{self.qty} - ${self.is_deleted} - {self.product.name}"
