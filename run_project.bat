bd3122e812f72130a75638c6af88f3c0f07d39e9
MengseuThoeng <mengseu2004@gmail.com>
2025-06-24 13:33:02 +0700

    run auto

---
+++ run_project.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Activating virtual environment...
+
+call venv\Scripts\activate.bat
+
+if not defined VIRTUAL_ENV (
+    echo ❌ Failed to activate virtual environment.
+    exit /b 1
+)
+
+echo ✅ Virtual environment activated.
+
+echo 🔄 Running makemigrations...
+python manage.py makemigrations
+
+echo 📦 Running migrate...
+python manage.py migrate
+
+echo 🎒 Collecting static files...
+python manage.py generate_fake_data
+
+echo 🚀 Starting development server...
+python manage.py runserver
